76027bcffd6a784263a5d06f6c876859e3fb27cf
eti.andre <eti.andre@gmail.com>
2019-06-04 14:31:14 +0200

    Player position debug window

--- Travelaws/Travelaws.cpp
+++ Travelaws/Travelaws.cpp
@@ -2,8 +2,7 @@
 //
 
 #pragma region includes
-#include "imgui-SFML.h"
-#include "imgui.h"
+#include "Travelaws.h"
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
@@ -14,8 +13,9 @@
 #include "InputManager.hpp"
 #include "Player.h"
 #include "SFMLOrthogonalLayer.hpp"
-#include "Travelaws.h"
 #include "WindRule.hpp"
+#include "imgui-SFML.h"
+#include "imgui.h"
 #pragma endregion includes
 
 Game::Game(int width, int height, std::string title) {
@@ -50,60 +50,65 @@ void Game::mainLoop() {
   rules.push_back(std::make_unique<CollisionRule>(
       platformsLayer));  //!\\ Collision has to be last !
 
-	sf::Clock clock;
+  sf::Clock clock;
 
   while (window.isOpen()) {
-			// EVENTS
-      while (window.pollEvent(event)) {
-        ImGui::SFML::ProcessEvent(event);
-        if (event.type == sf::Event::EventType::Closed) window.close();
-      }
-      ImGui::SFML::Update(window, clock.restart());
-
-      // GAME LOOP
-      // INPUTS
-      inputManager.manageInputs(gameData.player);
-
-      // PHYSICS UPDATE
-      for (auto& rule : rules) {
-        if (rule.get()->active) rule.get()->physicsUpdate(gameData);
-      }
-
-      // POSITION UPDATE
-      gameData.player.updatePosition();
-
-      // RULES UPDATE
-      for (auto& rule : rules) {
-        if (rule.get()->active) rule.get()->update(gameData);
-      }
-
-      // VIEW
-      view.reset(sf::FloatRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
-      sf::Vector2f position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-      position.x =
-          gameData.player.getPosition().x + BLOCK_SIZE / 2 - SCREEN_WIDTH / 2;
-      if (position.x < 0) position.x = 0;
-      if (position.y < 0) position.y = 0;
-
-      view.reset(sf::FloatRect(position.x, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
-      window.setView(view);
-
-			// GUI
-      ImGui::Begin("Rules");
-      for (auto& rule : rules) {
-        ImGui::Checkbox(rule.get()->getName(), &rule.get()->active);
-			}
-			ImGui::End(); // Rules
-
-      // DRAW
-      gameData.player.updateAnimation();
-      window.clear();
-      window.draw(backgroundLayer);
-      window.draw(platformsLayer);
-      window.draw(gameData.player.playerSprite);
-      ImGui::SFML::Render(window);
-      window.display();
+    // EVENTS
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
+      if (event.type == sf::Event::EventType::Closed) window.close();
     }
+    ImGui::SFML::Update(window, clock.restart());
+
+    // GAME LOOP
+    // INPUTS
+    inputManager.manageInputs(gameData.player);
+
+    // PHYSICS UPDATE
+    for (auto& rule : rules) {
+      if (rule.get()->active) rule.get()->physicsUpdate(gameData);
+    }
+
+    // POSITION UPDATE
+    gameData.player.updatePosition();
+
+    // RULES UPDATE
+    for (auto& rule : rules) {
+      if (rule.get()->active) rule.get()->update(gameData);
+    }
+
+    // VIEW
+    view.reset(sf::FloatRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    sf::Vector2f position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    position.x =
+        gameData.player.getPosition().x + BLOCK_SIZE / 2 - SCREEN_WIDTH / 2;
+    if (position.x < 0) position.x = 0;
+    if (position.y < 0) position.y = 0;
+
+    view.reset(sf::FloatRect(position.x, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    window.setView(view);
+
+    // GUI
+    ImGui::Begin("Rules");
+    for (auto& rule : rules) {
+      ImGui::Checkbox(rule.get()->getName(), &rule.get()->active);
+    }
+    ImGui::End();  // Rules
+
+    ImGui::Begin("Debug");
+    ImGui::Text("position : x=%d y=%d", gameData.player.getPosition().x, gameData.player.getPosition().y);
+    ImGui::Text("vitesse : x=%0.3f y=%0.3f", gameData.player.velocity.x, gameData.player.velocity.y);
+    ImGui::End();  // Debug
+
+    // DRAW
+    gameData.player.updateAnimation();
+    window.clear();
+    window.draw(backgroundLayer);
+    window.draw(platformsLayer);
+    window.draw(gameData.player.playerSprite);
+    ImGui::SFML::Render(window);
+    window.display();
+  }
 }
 
 int main(int argc, char** argv) {
