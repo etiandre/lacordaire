270288931348923d87829b2e93c6e30bd5ef8eb4
eti.andre <eti.andre@gmail.com>
2019-06-05 20:38:48 +0200

    fix bug collision sort du level

--- Travelaws/CollisionRule.cpp
+++ Travelaws/CollisionRule.cpp
@@ -1,11 +1,11 @@
 #include "CollisionRule.h"
+#include <cmath>
 #include "SFMLOrthogonalLayer.hpp"
 #include "tmxlite/Map.hpp"
-#include <cmath>
 
 // merci https://jonathanwhiting.com/tutorial/collision/
 
-CollisionRule::CollisionRule() {};
+CollisionRule::CollisionRule(){};
 
 void CollisionRule::update(GameData& gameData) {
   sf::FloatRect box;
@@ -27,23 +27,29 @@ void CollisionRule::update(GameData& gameData) {
   if (collides(box, map.getTileSize(), map.getTileCount(), collisionLayer)) {
     if (gameData.player.velocity.y > 0) {
       gameData.player.onGround = true;
-		}
+    }
     gameData.player.velocity.y = 0;
   }
 }
 
 bool CollisionRule::collides(const sf::FloatRect& box,
                              const tmx::Vector2u& tileSize,
-                             const tmx::Vector2u& tileCount, std::unique_ptr<MapLayer>& collisionLayer) {
+                             const tmx::Vector2u& tileCount,
+                             std::unique_ptr<MapLayer>& collisionLayer) {
+
+	if (box.left <= 0) return true;
   int topTile = box.top / tileSize.y;
   int leftTile = box.left / tileSize.x;
   int bottomTile = (box.top + box.height) / tileSize.y;
   int rightTile = (box.left + box.width) / tileSize.x;
+	
+	if (leftTile > tileCount.x) return false;
+  if (topTile > tileCount.y) return false;
 
   if (leftTile < 0) leftTile = 0;
   if (topTile < 0) topTile = 0;
-  if (rightTile > tileCount.x) rightTile = tileCount.x;
-  if (bottomTile > tileCount.y) rightTile = tileCount.y;
+  if (rightTile > tileCount.x - 1) rightTile = tileCount.x - 1;
+  if (bottomTile > tileCount.y - 1) rightTile = tileCount.y - 1;
 
   for (int i = leftTile; i <= rightTile; i++) {
     for (int j = topTile; j <= bottomTile; j++) {
