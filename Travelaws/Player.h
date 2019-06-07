51fd668c46a6f0702688148abab0d99cab422f4e
eti.andre <eti.andre@gmail.com>
2019-06-07 17:01:18 +0200

    piti commentaires

--- Travelaws/Player.h
+++ Travelaws/Player.h
@@ -13,10 +13,10 @@ class Player : public Actor {
 
  private:
   sf::Vector2i _animation;
-  float _airAcceleration;
-  float _groundAcceleration;
-  float _jumpAcceleration;
-  float _maxMoveSpeed;
-  float _groundFriction;
-  float _airFriction;
+  float _airAcceleration; // en pixels / s^2
+  float _groundAcceleration; // en pixels / s^2
+  float _jumpAcceleration; // en pixels / s^2
+  float _maxMoveSpeed; // en pixels / s
+  float _groundFriction; // coefficient entre 0 et 1: 0 = arrêt immédiat, 1 = pas de friction
+  float _airFriction; // idem
 };
