896fdfbafe998d5b88814e313d5170d8d21d27c3
AlgO <alexandre.brethes@gmail.com>
2019-06-07 16:54:26 +0200

    ye s yes yes

--- Travelaws/Player.cpp
+++ Travelaws/Player.cpp
@@ -6,52 +6,48 @@
 #include "Animation.hpp"
 
 Player::Player()
-    : Actor("Player"),
-      _maxMoveSpeed(180),
-      _groundAcceleration(18),
-      _airAcceleration(6),
-      _jumpAcceleration(100),
-      _groundFriction(0.5),
-      _airFriction(1) {
-  hitbox = sf::FloatRect(10, 22, 12, 10);
-  sf::Texture* texturePointer =
-      TextureManager::loadTexture("player", "assets/textures/slime.png");
-  if (texturePointer == NULL) {
-    std::cout << "erreur chargement texture slime !" << std::endl;
-    exit(1);
-  }
-  sprite.setTexture(*texturePointer);
- // sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
-  _anim.addAnimation(texturePointer, sf::Vector2i(10, 10), sf::seconds(0.25));
+	: Actor("Player"),
+	_maxMoveSpeed(180),
+	_groundAcceleration(18),
+	_airAcceleration(6),
+	_jumpAcceleration(100),
+	_groundFriction(0.5),
+	_airFriction(1) {
+	hitbox = sf::FloatRect(10, 22, 12, 10);
+	sf::Texture* texturePointer =
+		TextureManager::loadTexture("player", "assets/textures/slime.png");
+	if (texturePointer == NULL) {
+		std::cout << "erreur chargement texture slime !" << std::endl;
+		exit(1);
+	}
+	sprite.setTexture(*texturePointer);
+	// sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+	_anim.addAnimation(texturePointer, sf::Vector2i(10, 10), sf::seconds(0.25));
 }
 
 void Player::manageInputs(sf::Time dt) {
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-    if (onGround)
-      velocity.x -= _groundAcceleration;
-    else
-      velocity.x -= _airAcceleration;
-    if (velocity.x < -_maxMoveSpeed) velocity.x = -_maxMoveSpeed;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		if (onGround)
+			velocity.x -= _groundAcceleration;
+		else
+			velocity.x -= _airAcceleration;
+		if (velocity.x < -_maxMoveSpeed) velocity.x = -_maxMoveSpeed;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		if (onGround)
+			velocity.x += _groundAcceleration;
+		else
+			velocity.x += _airAcceleration;
+		if (velocity.x > _maxMoveSpeed) velocity.x = _maxMoveSpeed;
+	}
+	else {
+		if (onGround)
+			velocity.x *= _groundFriction;
+		else
+			velocity.x *= _airFriction;
+	}
 
-    animator(animatorState().x, 0);
-    animator(animatorState().x + 1, animatorState().y);
-  } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-    if (onGround)
-      velocity.x += _groundAcceleration;
-    else
-      velocity.x += _airAcceleration;
-    if (velocity.x > _maxMoveSpeed) velocity.x = _maxMoveSpeed;
-
-    animator(animatorState().x, 1);
-    animator(animatorState().x + 1, animatorState().y);
-  } else {
-    if (onGround)
-      velocity.x *= _groundFriction;
-    else
-      velocity.x *= _airFriction;
-  }
-
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && onGround) {
-    velocity.y = -_jumpAcceleration;
-  }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && onGround) {
+		velocity.y = -_jumpAcceleration;
+	}
 }
